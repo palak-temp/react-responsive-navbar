42b62e10ac3bee75c824977e72be20c9dc21e91b
Palak Trivedi <palakstrivedi@gmail.com>
2021-11-21 11:08:37 +0530

    main section updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Brand, Menu, CloseMenu } from "icons";
+import Presenter from "images/presenter.png";
 import clsx from "clsx";
 import "./App.css";
 
@@ -9,44 +10,55 @@ function App() {
   const [open, setOpen] = React.useState(false);
 
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Brand />
-        <span className="navbar-title">React Navbar</span>
-      </div>
-      <div className="navbar-burger" onClick={() => setOpen(!open)}>
-        {!open ? <Menu /> : <CloseMenu />}
-      </div>
-      <div className={clsx("navbar-collapse", { show: open })}>
-        <ul className={clsx("navbar-nav", { active: open })}>
-          <li className="navbar-item">
-            <a href="#" className="navbar-link">
-              Home
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="#" className="navbar-link">
-              About
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="#" className="navbar-link">
-              Contact
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="#" className="navbar-link">
-              Blog
-            </a>
-          </li>
-          <li className="navbar-item">
-            <a href="#" className="navbar-link">
-              Portfolio
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Brand />
+          <span className="navbar-title">React Navbar</span>
+        </div>
+        <div className="navbar-burger" onClick={() => setOpen(!open)}>
+          {!open ? <Menu /> : <CloseMenu />}
+        </div>
+        <div className={clsx("navbar-collapse", { show: open })}>
+          <ul className={clsx("navbar-nav", { active: open })}>
+            <li className="navbar-item">
+              <a href="#" className="navbar-link">
+                Home
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="#" className="navbar-link">
+                About
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="#" className="navbar-link">
+                Contact
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="#" className="navbar-link">
+                Blog
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="#" className="navbar-link">
+                Portfolio
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <main className="main">
+        <h1 className="header">React Responsive Navbar</h1>
+        <figure>
+          <img src={Presenter} alt="Presenter" className="presenter" />
+          <figcaption className="presenter-figcaption">
+            Image create gose to <a href="https://undraw.co/search">undraw</a>{" "}
+          </figcaption>
+        </figure>
+      </main>
+    </>
   );
 }
 
